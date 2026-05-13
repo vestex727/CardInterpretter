0f23710379d6312fd5505b158c9d99410848e763
vestex727 <smastrok9495@gmail.com>
2026-05-13 00:11:14 -0400

    Added functioning frontend folder

---
+++ Frontend/pagehandler.js
@@ -0,0 +1,21 @@
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function getCardList(cname) {
+    let s = getCookie(cname);
+    let cl = s.split('&')
+    return cl;
+}
