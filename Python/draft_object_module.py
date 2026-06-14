d30d7c6fefcab1447ceec8779151b1b50791d720
vestex727 <smastrok9495@gmail.com>
2026-06-14 13:16:36 -0400

    Filed out the room class with functions, including the ability to assign seats, let players join, and assign card passing order.

--- Python/draft_object_module.py
+++ Python/draft_object_module.py
@@ -1,4 +1,3 @@
-from os import name
 from queue import Queue
 from sympy import false, true
 
@@ -53,17 +52,18 @@ class Player:
     hand: Pile
     player_name: str
     player_id: int
+    player_number: int
+    next_player = None
 
-    def __init__(self, player_name: str, player_id: int, incoming_piles: Queue[Pile] = Queue[Pile](), hand: Pile = None):
+    def __init__(self, player_name: str, player_id: int, hand: Pile = None):
         """
         Creates a player object that has a name, if, hand of cards, and a queue of incoming card piles.
         :param player_name: Name of the player the object represents.
         :param player_id: ID of the player (important to allow for reconnection).
-        :param incoming_piles: a queue of piles coming towards this player.
         :param hand: A pile of cards in your hand. These are the only cards the client should have access to.
         """
         self.hand = hand
-        self.incoming_piles = incoming_piles
+        self.incoming_piles = Queue[Pile]()
         self.player_name = player_name
         self.player_id = player_id
         self.next_player = None
@@ -71,7 +71,7 @@ class Player:
             self.hand = self.incoming_piles.get()
 
     def __str__(self):
-        return "name: " + self.player_name + "\nid: " + str(self.player_id) + "\nhand: " + str(self.hand) + "incoming piles: " + str(self.incoming_piles.qsize())
+        return "name: " + self.player_name + "\nid: " + str(self.player_id) + "\nhand: " + str(self.hand) + "\nincoming piles: " + str(self.incoming_piles.qsize())
 
     def add_pile(self, pile: Pile) -> Pile:
         """
@@ -99,15 +99,15 @@ class Player:
         else:
             self.hand = None
 
-    def pick_n_pass(self, card_name, player):
+    def pick_n_pass(self, card_name, player_to_pass_to = next_player):
         """
         Removes the picked card, and then passes the pile to the next player
-        :param card_name:
-        :param player:
-        :return:
+        :param card_name: card to find, pick, and remove.
+        :param player_to_pass_to: player to pass the pile to after making the pick
+        :return: Nothing.
         """
         print(self.hand.remove_card_by_name(card_name))
-        self.send_pile(player)
+        self.send_pile(player_to_pass_to)
         print(self.hand)
 
 
@@ -116,6 +116,7 @@ class Room:
     player_count: int
     room_number: int
     passcode: str
+    taken_numbers: list[int]
 
     def __init__(self, player_count: int, room_number: int, passcode: str = "draft"):
         """
@@ -129,9 +130,57 @@ class Room:
         self.player_count = player_count
         self.room_number = room_number
         self.passcode = passcode
+        self.players = [None] * player_count
 
-    def let_player_join(self, player: Player):
+    def generate_seat_number(self):
+        """
+        Finds the lowest available seat number.
+        :return: lowest available seat number, or None if there are none available.
+        """
+        for i in range (1,self.player_count):
+            if not i in self.taken_numbers:
+                return i
+        return None
+
+    def let_player_join(self, player: Player, seat_number: int = -1):
+        """
+        Attempts to allow player to join the room.
+        If the room is full, it doesn't allow the player to join.
+        If the selected seat number the player wants is taken, fails to allow the player to join.
+        If the player number is -1, it allows the room to select the player's seat number.
+        :param player: The Player object to be put into the room.
+        :param seat_number: The seat number that the player is trying to take. If the seat is taken, they won't be able to join. If the value is -1, the seating will be assigned. Default value of -1.
+        :return: "success" if the player was able to join, "seat taken fail" if the desired seat was taken, and "full room fail" if the room was full.
+        """
         if len(self.players) < self.player_count:
-            self.players.append(player)
-            return "success"
-        return "fail"
+            if seat_number == -1 or seat_number in self.taken_numbers:
+                if seat_number == -1:
+                    seat_number = self.generate_seat_number()
+                self.players[seat_number-1] = player
+                return "success"
+            return "seat taken fail"
+        return "full room fail"
+
+    def assign_next_player_clockwise(self):
+        """
+        Assigns to each player in the room a "next player" in a "clockwise order" (1 -> 2, 2 -> 3,..., n-1 -> n, n -> 1).
+        Turns the players into a circular linked list.
+        :return: nothing
+        """
+        for i, player in self.players:
+            index = i + 1
+            if index == len(self.players):
+                index = 0
+            player.next_player = self.players[index]
+
+    def assign_next_player_counterclockwise(self):
+        """
+        Assigns to each player in the room a "next player" in a "counterclockwise order" (n -> n-1, n-1 -> n-2,..., 2 -> 1, 1 -> n).
+        Turns the players into a circular linked list.
+        :return: nothing
+        """
+        for i, player in reversed(self.players):
+            index = i + 1
+            if index == len(self.players):
+                index = 0
+            player.next_player = self.players[index]
